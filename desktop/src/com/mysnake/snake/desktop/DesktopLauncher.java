398fa00d12da96641a98d90b50e8f2c4e0a07af9
Dominick Burns <dominickb613@gmail.com>
2018-03-29 16:46:22 -0400

    Added More Files

---
+++ desktop/src/com/mysnake/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mysnake.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mysnake.snake.Snake;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snake(), config);
+	}
+}
